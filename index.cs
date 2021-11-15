2e9cfad563317be79bc297b7d6995cf8c6dba934
ShieldedDemon19 <88759733+ShieldedDemon19@users.noreply.github.com>
2021-11-15 09:29:22 -0500

    +, -, and $ operations added
    more functionality that no one will use!

--- index.cs
+++ index.cs
@@ -47,7 +47,7 @@ namespace TWO
 			int a = 0; //first variable, used for data, etc.
 			int b = 0; //second variable, used for functions and events, etc.
 			int operand = -1;
-            int ks = 0;
+			int ks = 0;
 			
 			for(int k = 0; k < data.Length; k++)
 			{
@@ -83,20 +83,36 @@ namespace TWO
 					case '!': //tell interpreter to do something
 						operand = b;
 						break;
-                        
-                    case '[':
-                        Loop = true;
-                        ks = k;
-                        break;
-                        
-                    case ']':
-                        if(Loop && b >= 0){
-                            k = ks;
-                        }else{
-                            Loop = false;
-                        }
-                        break;
 						
+					case '[': //start loop
+						Loop = true;
+						ks = k;
+						break;
+					
+					case ']': //end loop
+						if(Loop && b >= 0){
+							k = ks;
+						}else{
+							Loop = false;
+						}
+						break;
+					
+					case '+': //add operator
+						a = a+b;
+						break;
+					
+					case '-': //subtract operator
+						a = a-b;
+						break;
+					
+					case '$': //mula mula operator 
+						{
+							int temp = a+b;
+							a = temp;
+							b = temp;
+						}
+						break;
+					
 					default: //invalid characters are skipped
 						operand = -1;
 						break;
@@ -160,6 +176,10 @@ namespace TWO
 	! - call function based on b's current value
 	[ - start loop
 	] - end loop
+	+ - add a and b and store in a
+	- - subtract a and b and store in a
+	$ - add a and b and store in both
+	more operations coming soon!
 	
 	0 - end program
 	1 - output character in a 
